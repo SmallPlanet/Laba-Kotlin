9b3ec52f0382a6abdbe7dbe70b2d959886f1222a
Javier Moreno <javier.moreno@smallplanet.com>
2017-05-26 11:10:55 -0400

    Refactoring the extensions package.

--- labalib/src/main/java/com/smallplanet/labalib/Extensions.kt
+++ labalib/src/main/java/com/smallplanet/labalib/Extensions.kt
@@ -6,24 +6,9 @@ import android.content.res.Resources
  * Created by javiermoreno on 5/23/17.
  */
 
-val Float.toDp: Float
-    get(){
-        return (this - 0.5f) / Resources.getSystem().displayMetrics.density
-    }
-
-val Int.toDp: Int
-    get(){
-        return ((this - 0.5f) / Resources.getSystem().displayMetrics.density).toInt()
-    }
-
 val Float.toPx: Float
     get(){
         return Resources.getSystem().displayMetrics.density * this + 0.5f
     }
 
-val Int.toPx: Int
-    get(){
-        return (Resources.getSystem().displayMetrics.density * this + 0.5f).toInt()
-    }
-
-fun Number.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
+fun Number.format(digits: Int) = java.lang.String.format("%.${digits}f", this)!!
